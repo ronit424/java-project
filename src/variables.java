8c0dd72f44123961d09e157eaacffb989f97c0ad
Ronit <ronitshah2004@gmail.com>
2020-03-22 18:38:34 +0545

    variable program

---
+++ src/variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[]args) {
+        short my_variable = 10;
+        float my_decimal = (float) 4.5;
+        double my_double = 11.5;
+        char my_char = 'A';
+        boolean is_true = false;
+        System.out.println(my_variable);
+        System.out.println(my_decimal);
+        System.out.println(my_double);
+        System.out.println(my_char);
+        System.out.println(is_true);
+    }
+}
