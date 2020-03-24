3e221f17a87506904697f3a925dece7c1c9193cc
Ronit <ronitshah2004@gmail.com>
2020-03-24 08:31:27 +0545

    mythirdprogram

---
+++ src/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("enter some number");
+        int user_input_number = scan.nextInt();
+
+        System.out.println("the entered value is");
+        System.out.println(user_input_number);
+    }
+}
