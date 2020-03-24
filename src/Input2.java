26592753b0b97fcbdee6b75643ed7fbe9aba16a7
Ronit <ronitshah2004@gmail.com>
2020-03-24 08:47:48 +0545

    Input2.java

---
+++ src/Input2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Input2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("enter some number");
+        int user_input_number = scan.nextInt();
+
+        System.out.println("so the entered value is");
+        System.out.print(user_input_number);
+
+        Scanner scan1 = new Scanner (System.in);
+        System.out.println("enter some decimal value");
+        double user_input_double = scan1.nextDouble();
+
+        System.out.println("so the entered value is");
+        System.out.print(user_input_double);
+
+    }
+}
