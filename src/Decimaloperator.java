01250416b56ab78fd4180f4a519e820cc3b5cfd8
Ronit <ronitshah2004@gmail.com>
2020-03-24 08:56:00 +0545

    Decimaloperator.java

---
+++ src/Decimaloperator.java
@@ -0,0 +1,10 @@
+public class Decimaloperator {
+    public static void  main(String[]args){
+        int x , y , answer;
+        x=70;
+        y=30;
+        answer=x / y;
+        System.out.println("answer="+ answer);
+
+    }
+}
