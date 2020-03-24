393b88f814a165e31864421fb9a1c0da03008160
Ronit <ronitshah2004@gmail.com>
2020-03-24 08:58:55 +0545

    Remindergiver.java

--- src/Remindergiver.java
+++ src/Remindergiver.java
@@ -1,9 +1,9 @@
-public class Decimaloperator {
+public class Remindergiver {
     public static void  main(String[]args){
         int x , y , answer;
-        x=70;
+        x=80;
         y=30;
-        answer=x / y;
+        answer=x % y;
         System.out.println("answer="+ answer);
 
     }
