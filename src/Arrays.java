70650ec810304cea17b2f2ea9ffe6fbd29722597
Ronit <ronitshah2004@gmail.com>
2020-03-25 19:45:54 +0545

    Arrays.java

---
+++ src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] myintarray ={100,31,26,48,52};
+        int index = 0;
+        while(index < 5){
+            System.out.println(myintarray[index]);
+            index++;
+        }
+    }
+}
