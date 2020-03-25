812c5fbce34533ad13fab6f113e8bd58b68b8992
Ronit <ronitshah2004@gmail.com>
2020-03-25 18:59:05 +0545

    And.java

---
+++ src/Condition.java
@@ -0,0 +1,11 @@
+public class Condition {
+    public static void main(String[] args) {
+        int subject1 = 20;
+        int subject2 = 60;
+        if((subject1 >= 35) && (subject2 >= 35)) {
+            System.out.println("the condition is true");
+        }else {
+            System.out.println("the condition i false");
+        }
+    }
+}
