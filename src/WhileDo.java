cd5addc5944f74eb2bb23a308f2c74f8b07f1cda
Ronit <ronitshah2004@gmail.com>
2020-03-25 19:36:28 +0545

    WhileDo.java

---
+++ src/WhileDo.java
@@ -0,0 +1,16 @@
+public class WhileDo {
+    public static void main(String[] args) {
+        int a = 0;
+                while(a <= 10)
+                {
+                    System.out.println(a);
+                    a++;
+                }
+        System.out.println("------------------------------------");
+                int b = 0;
+                do{
+                    System.out.println(b);
+                    b++;
+                }while (b <= 10);
+    }
+}
