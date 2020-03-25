c740eee39c8c6082c71d7059b7f3f1e1eaa60ef6
Ronit <ronitshah2004@gmail.com>
2020-03-25 20:05:08 +0545

    loop2.java

--- src/loop2.java
+++ src/loop2.java
@@ -1,6 +1,6 @@
 public class loop2 {
     public static void main(String[] args) {
-        int[] myintarry = {100,31,26,48,52};
+        int[] myintarry = {100,31,26,49,52};
         for(int index=0 ; index < 5 ; index++)
         {
             System.out.println(myintarry[index]);
