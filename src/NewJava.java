b9af83a3067bc16b481a9d98fab52111a415cb72
Ronit <ronitshah2004@gmail.com>
2020-03-25 21:52:41 +0545

    NewJava

---
+++ src/NewJava.java
@@ -0,0 +1,8 @@
+public class NewJava {
+    public static void main(String[] args) {
+        String mystring = "Hello e world e";
+        int myStringlegth = mystring.length();
+        String myStringincase = mystring.toUpperCase();
+        System.out.println(mystring.indexOf('o'));
+    }
+}
