96c1c6ec7bda25041901eb699a7ebc6e9f7cdcf7
Ronit <ronitshah2004@gmail.com>
2020-03-26 09:10:14 +0545

    Loop3

--- src/Loop.java
+++ src/Loop.java
@@ -1,6 +1,6 @@
 public class Loop {
     public static void main(String[] args) {
-        for (int i=0; i<5; i++){
+        for (int i=0; i<10; i++){
             System.out.println("the value of i = " +i);
         }
     }
