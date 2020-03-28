26bbce145a10e7b130eff3116fe7776e614ad889
Ronit <ronitshah2004@gmail.com>
2020-03-28 09:25:19 +0545

    Perimeter

---
+++ src/Perimeter2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Perimeter2 {
+    public static void main(String args[])
+    {
+
+        Scanner s= new Scanner(System.in);
+
+        System.out.println("Enter the radius:");
+
+        double r= s.nextDouble();
+
+        double  c=(22*2*r)/7 ;
+
+        System.out.println("Perimeter of Circle is: " +c);
+    }
+}
