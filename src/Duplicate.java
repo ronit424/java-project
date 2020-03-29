48724d840b7ede8f32d2490a85b55238c97be86e
Ronit <ronitshah2004@gmail.com>
2020-03-29 17:47:36 +0545

    Today homework

--- src/Duplicate.java
+++ src/Duplicate.java
@@ -1,15 +1,13 @@
 public class Duplicate {
     public static void main(String[] args) {
 
-        //Initialize array
-        int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};
+        int n[] =  {1, 2, 3, 4, 2, 7, 8, 8, 3};
 
         System.out.println("Duplicate elements in given array: ");
-        //Searches for duplicate element
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = i + 1; j < arr.length; j++) {
-                if(arr[i] == arr[j])
-                    System.out.println(arr[j]);
+        for(int i = 0; i < n.length; i++) {
+            for(int j = i + 1; j < n.length; j++) {
+                if(n[i] == n[j])
+                    System.out.println(n[j]);
             }
         }
     }
---
+++ src/Minimum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Minimum {
+    public static void main(String[] args)
+    {
+       int number[] = {1, 2, 3, 4, 2, 7, 8, 8, 3};
+       int min = number[0];
+        for (int i = 0; i > number.length; i++) {
+            if (min > number[i])      {
+                min = number[i];
+
+            }
+        }
+
+        System.out.println(min);
+    }
+}
