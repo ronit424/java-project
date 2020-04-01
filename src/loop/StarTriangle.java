960a363001f6ef5dec5b80717378a6b2e2ecbc6d
Ronit <ronitshah2004@gmail.com>
2020-04-01 20:14:19 +0545

    Pattern

---
+++ src/loop/StarTriangle.java
@@ -0,0 +1,39 @@
+package loop;
+
+/*
+1. print below pattern
+*****
+****
+***
+**
+*
+
+2. print below pattern
+*
+**
+***
+****
+*****
+
+
+ */
+
+public class StarTriangle {
+    public static void printStars(int n)
+    {
+        int i, j;
+        for(i=0; i<n; i++)
+    {
+        for(j=0; j<=i; j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+    public static void main(String args[])
+    {
+        int n = 5;
+        printStars(n);
+    }
+}
