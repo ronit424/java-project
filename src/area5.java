56c1c57a37ba78089a9e65957ea502782875b9fc
Ronit <ronitshah2004@gmail.com>
2020-04-04 09:13:25 +0545

    TSA of cuboid

---
+++ src/area5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class area5 {
+    public static void main(String args[])
+    {
+
+
+
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter the length of Cubiod:");
+        double l=s.nextDouble();
+
+        System.out.println("Enter the breadth of Cubiod:");
+        double b=s.nextDouble();
+
+        System.out.println("Enter height of Cubiod:");
+        double h=s.nextDouble();
+
+        double  area=(2*((l*b)+(b*h)+(h*l)));
+
+        System.out.println("Total SurfaceArea Of Cuboid is:" +area);
+
+    }
+}
